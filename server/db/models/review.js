51e4538cc723eeb74fae8653741474703cbd40ee
Hailey Keen <haileykeen@Haileys-MacBook-Pro.local>
2016-03-01 11:45:59 -0500

    model for order and reviews

---
+++ server/db/models/review.js
@@ -0,0 +1,24 @@
+'use strict';
+var mongoose = require('mongoose');
+var _ = require('lodash');
+
+var schema = new mongoose.Schema({
+  rating: {
+    type: Number,
+    default: null
+  },
+  content: {
+    type: String,
+    minlength: 5
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product'
+  }
+});
+
+mongoose.model('Review', schema);
