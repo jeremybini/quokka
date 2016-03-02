8316c80fb4fc8ae557c96ebcd43416264f71efc9
Liana <lrc47@cornell.edu>
2016-03-02 11:54:40 -0500

    started product model testing

---
+++ tests/server/models/product-test.js
@@ -0,0 +1,13 @@
+/* PRODUCT MODEL TESTS */
+var dbURI = 'mongodb://localhost:27017/testingDB';
+var clearDB = require('mocha-mongoose')(dbURI);
+
+var sinon = require('sinon');
+var expect = require('chai').expect;
+var mongoose = require('mongoose');
+
+// Require in all models.
+require('../../../server/db/models');
+
+var Product = mongoose.model('Product');
+
