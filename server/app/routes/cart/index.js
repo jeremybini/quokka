36235770a08f67de9f4f4fc1c1d53f323b989b77
John Rauschenberg <john.rauschenberg@gmail.com>
2016-03-02 12:43:00 -0500

    Added sumit function to router.

--- server/app/routes/cart/index.js
+++ server/app/routes/cart/index.js
@@ -23,6 +23,9 @@ router.use(function(req, res, next) {
   .then(null, next);
 });
 
+//what happens when unauth users add to carts then leave? we will
+//have lots of carts in the database that should be removed
+
 //req.body should have a product ID
 router.get('/remove', function(req, res, next) {
   req.cart.products = req.cart.products.filter(function(item) {
@@ -47,6 +50,17 @@ router.get('/add', function(req, res, next) {
 router.get('/update', function(req, res, next) {
   var changingProduct = _.find(req.cart.products, {product: req.body.productId});
   changingProduct.quantity = req.body.quantity;
+  req.cart.save()
+  .then(function(result) {
+    res.status(204).json(result);
+  })
+  .then(null, next);
+});
+//req.body should have a product ID, updated status--this is where orders
+//are actually submitted
+//at this time, confirmation email should be sent and other actions probably triggered
+router.get('/submit', function(req, res, next) {
+  req.cart.status = "Submitted";
   req.cart.save()
   .then(function(result) {
     res.status(204).json(result);
