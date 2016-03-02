82b6c714108aeec2f9eafb1aeb92290bf662736c
John Rauschenberg <john.rauschenberg@gmail.com>
2016-03-02 12:19:21 -0500

    Added cart router functionality and updated order schema.

--- server/app/routes/cart/index.js
+++ server/app/routes/cart/index.js
@@ -4,19 +4,53 @@ module.exports = router;
 var mongoose = require('mongoose');
 var Order = mongoose.model('Order');
 var User = mongoose.model('User');
+var _ = require('lodash');
 
-router.get('/add', function(req, res, next) {
-	if (req.user) {
-
-	} else {
-
-	}
+//this gets run whenever user adds or updates a cart, or is a new user and needs a new cart
+router.use(function(req, res, next) {
+  var params = {
+    status: 'Cart'
+  };
+  if (req.user) {
+    (params.user = req.user._id);
+  } else {
+    (params.session = req.sessionID);
+  }
+  Order.findOrCreate(params)
+  .then(function(order) {
+    req.cart = order;
+  })
+  .then(null, next);
 });
 
+//req.body should have a product ID
 router.get('/remove', function(req, res, next) {
-
+  req.cart.products = req.cart.products.filter(function(item) {
+    return item.product !== req.body.productId;
+  });
+  req.cart.save()
+  .then(function(result) {
+    res.status(204).json(result);
+  })
+  .then(null, next);
 });
-
+//req.body should have a product ID
+router.get('/add', function(req, res, next) {
+  req.cart.products.push(req.body.productId);
+  req.cart.save()
+  .then(function(result) {
+    res.status(204).json(result);
+  })
+  .then(null, next);
+});
+//req.body should have a product ID, updated quantity
 router.get('/update', function(req, res, next) {
-
+  var changingProduct = _.find(req.cart.products, {product: req.body.productId});
+  changingProduct.quantity = req.body.quantity;
+  req.cart.save()
+  .then(function(result) {
+    res.status(204).json(result);
+  })
+  .then(null, next);
 });
+
