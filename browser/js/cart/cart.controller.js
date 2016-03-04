d9dd3c39d42e473aff124d435b29985aac2cc46d
Liana <lrc47@cornell.edu>
2016-03-04 12:12:00 -0500

    started cart controller and cart template

---
+++ browser/js/cart/cart.controller.js
@@ -0,0 +1,17 @@
+//CART CONTROLLER
+app.controller('CartController', function($scope, CartFactory) {
+  $scope.cart = CartFactory.getCart();
+
+  $scope.remove = function(productId) {
+    return CartFactory.remove(productId);
+  };
+
+  $scope.update = function(productId, quantity) {
+    return CartFactory.update(productId, quantity);
+  };
+
+  $scope.empty = function() {
+    return CartFactory.empty();
+  };
+
+});
